f2c1c82e84a1bb4c105ff114ade325ddefac995c
Jayson Exel <contact@jayexel.com>
2016-04-06 15:07:53 -0400

    Update scrollTop.initialize.js
    Fix jQuery noComflict();

--- js/scrollTop.initialize.js
+++ js/scrollTop.initialize.js
@@ -1,5 +1,5 @@
-$(function () {
-	j$ = jQuery.noConflict();
+var j$ = jQuery.noConflict();
+j$(function () {
     j$('#scrolltoTop').topScroll({
 		backgroundColor : scroll_settings.backgroundColor,
 		borderRadius : scroll_settings.borderRadius
